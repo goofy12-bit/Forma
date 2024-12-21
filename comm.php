7c04ae6c17122d3e04558385bbb2ee98dc8f987a
goofy12 <155470090+goofy12-bit@users.noreply.github.com>
2024-12-21 14:11:34 +0400

    Update comm.php

--- comm.php
+++ comm.php
@@ -3,7 +3,7 @@ if (isset($_REQUEST['ok'])) {
  
     $string = '<p>'.$_REQUEST['username'].'<br>'.$_REQUEST['msg'];
     file_put_contents('1.txt', $string, FILE_APPEND);
-    header("Location: /index.html");
+    header("Location: /Nol.html");
     exit();
 }
 include ('1.txt');
